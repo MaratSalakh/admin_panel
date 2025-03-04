f0d3baadf8d29e6886e35a85e602c899934ccd2d
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-04 22:43:14 +0300

    add component

---
+++ src/components/customUI/warningText/WarningText.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { CircleHelp } from "lucide-vue-next";
+</script>
+
+<template>
+  <div class="flex max-w-max justify-start gap-2 rounded-md bg-blue-50 p-2">
+    <CircleHelp />Для указания нескольких меток для одной пары логин/пароль
+    используйте разделитель ;
+  </div>
+</template>
+
+<style scoped></style>
