440c77acf04b0a050cd15685638fb701fd6c23b1
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-05 13:49:45 +0300

    creating table

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Plus } from "lucide-vue-next";
+import Button from "./components/ui/button/Button.vue";
+import WarningText from "./components/customUI/warningText/WarningText.vue";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex justify-center">
+    <div class="flex w-9/12 flex-col gap-4 p-2 align-middle">
+      <div class="flex justify-start gap-2 align-middle text-2xl">
+        <h2>Учетные записи</h2>
+        <Button><Plus /></Button>
+      </div>
+
+      <WarningText />
+
+      <Table>
+        <TableCaption>Таблица</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead class="w-[100px]"> Метки </TableHead>
+            <TableHead>Тип записи</TableHead>
+            <TableHead>Логин</TableHead>
+            <TableHead class="text-right"> Пароль </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell class="font-medium"> XXX </TableCell>
+            <TableCell> Локальная </TableCell>
+            <TableCell> Значение </TableCell>
+            <TableCell class="text-right"> Пароль </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
