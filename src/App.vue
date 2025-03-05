cffb43c13fa505903adbd3dc975b772607c59f56
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-05 14:33:33 +0300

    add select to app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Plus } from "lucide-vue-next";
+import { Plus, Trash2 } from "lucide-vue-next";
 import Button from "./components/ui/button/Button.vue";
 import WarningText from "./components/customUI/warningText/WarningText.vue";
 import {
@@ -11,6 +11,16 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
+import { Input } from "./components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 </script>
 
 <template>
@@ -27,18 +37,35 @@ import {
         <TableCaption>Таблица</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead class="w-[100px]"> Метки </TableHead>
-            <TableHead>Тип записи</TableHead>
+            <TableHead> Метки </TableHead>
+            <TableHead class="w-[200px]">Тип записи</TableHead>
             <TableHead>Логин</TableHead>
-            <TableHead class="text-right"> Пароль </TableHead>
+            <TableHead> Пароль </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell class="font-medium"> XXX </TableCell>
-            <TableCell> Локальная </TableCell>
-            <TableCell> Значение </TableCell>
-            <TableCell class="text-right"> Пароль </TableCell>
+            <TableCell class="font-medium"> <Input /> </TableCell>
+            <TableCell>
+              <Select>
+                <SelectTrigger class="w-[180px]">
+                  <SelectValue placeholder="Тип записи" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="apple"> LDAP </SelectItem>
+                    <SelectItem value="banana"> Локальная </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </TableCell>
+            <TableCell> <Input /> </TableCell>
+            <TableCell> <Input type="password" /> </TableCell>
+            <TableCell>
+              <Button variant="ghost">
+                <Trash2 />
+              </Button>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
