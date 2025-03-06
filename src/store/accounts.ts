849798c27449a552e11e7beed633723a376bccad
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-07 00:16:26 +0300

    create store

---
+++ src/store/accounts.ts
@@ -0,0 +1,28 @@
+import type { Account } from "@/models/account";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useAccountsStore = defineStore("accounts", () => {
+  const counter = ref(0);
+
+  const accountsList = ref<Account[]>([]);
+
+  // Метод для сохранения состояния в localStorage
+  function saveToLocalStorage() {
+    localStorage.setItem("accounts", JSON.stringify(accountsList.value));
+  }
+
+  // Метод для загрузки состояния из localStorage
+  function loadFromLocalStorage() {
+    const accounts = localStorage.getItem("accounts");
+    if (accounts) {
+      accountsList.value = JSON.parse(accounts);
+    }
+  }
+  return {
+    counter,
+    accountsList,
+    saveToLocalStorage,
+    loadFromLocalStorage,
+  };
+});
