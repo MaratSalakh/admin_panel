92d82bf1c35d61ffa33b4c8c0d28b9d4161047c1
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-07 00:16:10 +0300

    create store

---
+++ src/models/account.ts
@@ -0,0 +1,10 @@
+export type Account = {
+  id: number;
+  mark: string;
+  validateMark: boolean;
+  accountType: string | null;
+  login: string;
+  validateLogin: boolean;
+  password: string;
+  validatePassword: boolean;
+};
